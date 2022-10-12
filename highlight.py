5c027af8de978d0c614db2811ce281cdb017c453
Ralph Smith <ralph@ralphsmithphoto.com>
2022-10-12 09:17:09 -0500

    refactored codeblocks into functions: load_code and tokenizer

--- highlight.py
+++ highlight.py
@@ -1,12 +1,17 @@
-'''A module to generate display code for my website'''
+'''
+A module to generate display code for my website
+input: a file containing properly formatted html
+output: a file with css tags added
+'''
 import d_stack
-DEBUG = True
+DEBUG = False
 
 
 _PAIRS = {
     '<' : '>',
     '"' : '"',
 }
+
 
 _SINGLE = set(["="," "])
 
@@ -16,47 +21,77 @@ _HTMLSAFE = {
     '>' : "&gt",
 }
 
-code_filename="html_to_convert2.html"
-lines = list()
 
-with open(code_filename, "r") as f:
-    l = f.readline()
-    while l:
-        lines.append(l.rstrip())
+# load in data
+def load_code(code_file: str):
+    '''Loads a code file'''
+    lines = list()  # holds all the lines
+    l = list()      # holds a single lines
+    with open(code_file, "r") as f:
         l = f.readline()
+        while l:
+            lines.append(l.rstrip())
+            l = f.readline()
+    return lines
 
-out_text = []
-for i, line in enumerate(lines):
-    out_line = list()
-    tokens = d_stack.D_Stack()
-    matched_command=d_stack.D_Stack()
-    working = ""
-    for j, c in enumerate(line):
-        if c == matched_command.top():
-            out_line.append(working)
-            out_line.append(c)
-            matched_command.pop()
-            working = ""
-        elif c in _SINGLE or c in _PAIRS.keys():
-            if working:
-                out_line.append(working)
-            working = ""
-            out_line.append(c)
 
-            if c in _PAIRS.keys():
-                matched_command.push(_PAIRS[c])
-        else:
-            working += c
+# run tokenizer
+def tokenizer(lines: list):
+    '''Takes in a list of lines : str, returns a list of line tokens'''
+    tokens = []  # will hold the line_tokens
+    for i, line in enumerate(lines):
+        line_tokens = list()
+        matched_command=d_stack.D_Stack()
+        working = ""
+        for j, c in enumerate(line):
+            if c == matched_command.top():
+                line_tokens.append(working)
+                line_tokens.append(c)
+                matched_command.pop()
+                working = ""
+            elif c in _SINGLE or c in _PAIRS.keys():
+                if working:
+                    line_tokens.append(working)
+                working = ""
+                line_tokens.append(c)
 
-        if DEBUG:
-            print(f"line iteration {i} and char iteration {j} current char {c}")
-            print(f"outline {out_line}")
-            print(f"tokens {tokens}")
-            print(f"matched_command {matched_command}")
-            print(F"working {working}")
-            print()
+                if c in _PAIRS.keys():
+                    matched_command.push(_PAIRS[c])
+            else:
+                working += c
 
-    out_text.append(out_line)
+            if DEBUG:
+                print(f"line iteration {i} and char iteration {j} current char {c}")
+                print(f"outline {line_tokens}")
+                print(f"tokens {tokens}")
+                print(f"matched_command {matched_command}")
+                print(F"working {working}")
+                print()
 
-print(f"out_text: {out_text[0]}")
-print(F"joined: {''.join(out_text[0])}")
+        tokens.append(line_tokens)
+
+    return tokens
+
+
+
+
+
+def program(file_name):
+    lines = load_code(file_name)
+    tokens = tokenizer(lines)
+
+    for line in tokens:
+        print(f"tokens: {line}")
+        print(F"joined: {''.join(line)}")
+        print()
+
+
+def main():
+    '''Run a test'''
+    code_filename="html_to_convert.html"
+    program(code_filename)
+
+
+
+if __name__ == "__main__":
+    main()
